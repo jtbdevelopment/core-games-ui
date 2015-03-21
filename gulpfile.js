ba216ec2347f1848abfb910590a4ee7f23365213
JTB Development <jtbdevelopment@gmail.com>
2015-03-21 18:47:23 -0400

    switching bumper/tagger

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ var runSequence = require('run-sequence');
 var jshint = require('gulp-jshint');
 var bump = require('gulp-bump');
 var git = require('gulp-git');
+var filter = require('gulp-filter');
+var tag_version = require('gulp-tag-version');
 
 /**
  * File patterns
@@ -35,25 +37,41 @@ var lintFiles = [
   'karma-*.conf.js'
 ].concat(sourceFiles);
 
-gulp.task('tag', function () {
-  var pkg = require('./package.json');
-  var v = 'v' + pkg.version;
-  var message = 'Release ' + v;
+/**
+ * Bumping version number and tagging the repository with it.
+ * Please read http://semver.org/
+ *
+ * You can use the commands
+ *
+ *     gulp patch     # makes v0.1.0 → v0.1.1
+ *     gulp feature   # makes v0.1.1 → v0.2.0
+ *     gulp release   # makes v0.2.1 → v1.0.0
+ *
+ * To bump the version numbers accordingly after you did a patch,
+ * introduced a feature or made a backwards-incompatible release.
+ */
 
-  return gulp.src('./')
-    .pipe(git.commit(message))
-    .pipe(git.tag(v, message))
-    .pipe(git.push('origin', 'master', '--tags'))
-    .pipe(gulp.dest('./'));
-});
-
-gulp.task('bump', function () {
+function inc(importance) {
+  // get all the files to bump version in
   return gulp.src(['./package.json', './bower.json'])
-    .pipe(bump())
-    .pipe(gulp.dest('./'));
-});
+    // bump the version number in those files
+    .pipe(bump({type: importance}))
+    // save it back to filesystem
+    .pipe(gulp.dest('./'))
+    // commit the changed version number
+    .pipe(git.commit('bumps package version'))
 
-gulp.task('build', function() {
+    // read only one file to get the version number
+    .pipe(filter('package.json'))
+    // **tag it in the repository**
+    .pipe(tag_version());
+}
+
+gulp.task('patch', function() { return inc('patch'); });
+gulp.task('feature', function() { return inc('minor'); });
+gulp.task('release', function() { return inc('major'); });
+
+gulp.task('build', function () {
   gulp.src(sourceFiles)
     .pipe(plumber())
     .pipe(concat('core-games-ui.js'))

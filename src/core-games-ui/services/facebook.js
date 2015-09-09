c592c8591597f0a7bdf3b80ffa99fe38e9bfe952
JTB <jtbdevelopment@gmail.com>
2015-09-09 06:56:08 -0400

    updated facebook lib to support ng-cordova/facebookconnectplugin if on a file system (i.e. cordova)

--- src/core-games-ui/services/facebook.js
+++ src/core-games-ui/services/facebook.js
@@ -2,45 +2,56 @@
 'use strict';
 
 angular.module('coreGamesUi.services').factory('jtbFacebook',
-    ['$http', '$location', '$q', '$rootScope',
-        function ($http, $location, $q, $rootScope) {
+    ['$http', '$location', '$q', '$injector', '$window',
+        function ($http, $location, $q, $injector, $window) {
             var loaded = false;
             var facebookAppId = '';
             var facebookPermissions = '';
 
-            //  TODO - deal with facebook disconnect and such
+            var cordovaFacebook;
+            try {
+                if ($window.location.href.indexOf('file:') === 0) {
+                    cordovaFacebook = $injector.get('$cordovaFacebook');
+                }
+            } catch(ex) {
+                cordovaFacebook = undefined;
+            }
+
             function loadFB() {
                 var fbLoaded = $q.defer();
                 if (!loaded) {
                     $http.get('/api/social/apis', {cache: true}).success(function (response) {
                         facebookAppId = response.facebookAppId;
                         facebookPermissions = response.facebookPermissions;
-                        window.fbAsyncInit = function () {
-                            FB.init({
-                                appId: facebookAppId,
-                                xfbml: false,
-                                version: 'v2.2'
-                            });
-                            fbLoaded.resolve(facebookPermissions);
-                        };
+                        if(angular.isDefined(cordovaFacebook)) {
+                            fbLoaded.resolve();
+                        } else {
+                            window.fbAsyncInit = function () {
+                                FB.init({
+                                    appId: facebookAppId,
+                                    xfbml: false,
+                                    version: 'v2.2'
+                                });
+                                fbLoaded.resolve();
+                            };
 
-                        (function (d, s, id) {
-                            function onLoadCB() {
-                                loaded = false;
-                                fbLoaded.reject();
-                            }
+                            (function (d, s, id) {
+                                function onLoadCB() {
+                                    loaded = false;
+                                    fbLoaded.reject();
+                                }
 
-                            var js, fjs = d.getElementsByTagName(s)[0];
-                            if (d.getElementById(id)) {
-                                return;
-                            }
-                            js = d.createElement(s);
-                            js.id = id;
-                            js.src = '//connect.facebook.net/en_US/sdk.js';
-                            js.onerror = onLoadCB;
-                            fjs.parentNode.insertBefore(js, fjs);
-                        }(document, 'script', 'facebook-jssdk'));
-
+                                var js, fjs = d.getElementsByTagName(s)[0];
+                                if (d.getElementById(id)) {
+                                    return;
+                                }
+                                js = d.createElement(s);
+                                js.id = id;
+                                js.src = '//connect.facebook.net/en_US/sdk.js';
+                                js.onerror = onLoadCB;
+                                fjs.parentNode.insertBefore(js, fjs);
+                            }(document, 'script', 'facebook-jssdk'));
+                        }
                         loaded = true;
                     }).error(function () {
                         $location.path('/error');
@@ -54,45 +65,167 @@ angular.module('coreGamesUi.services').factory('jtbFacebook',
                 }
             }
 
-            //  eventName - string
-            //  params - object with
-            $rootScope.$on('event', function (eventName, countable, otherData) {
-                var data = otherData;
-                if (!angular.isDefined(data)) {
-                    data = {};
-                }
-                var count = countable;
-                if (!angular.isDefined(countable)) {
-                    count = 1;
-                }
+            function login(fbLogin) {
                 try {
-                    FB.AppEvents.logEvent(eventName, count, data);
-                } catch (err) {
-                    console.debug('Failed to log to FB ' + err);
+                    var callback = function(response) {
+                        if (angular.isDefined(response) &&
+                            angular.isDefined(response.status) &&
+                            response.status === 'connected') {
+                            fbLogin.resolve({
+                                auto: true,
+                                permissions: facebookPermissions
+                            });
+                        } else {
+                            fbLogin.reject();
+                        }
+                    };
+
+                    if(angular.isDefined(cordovaFacebook)) {
+                        cordovaFacebook.login(facebookPermissions.split(',')).then(callback,
+                            function(e) {
+                                console.log(JSON.stringify(e));
+                                fbLogin.reject();
+                            });
+                    } else {
+                        FB.login(callback, {scope: facebookPermissions});
+                    }
+                } catch (ex) {
+                    console.error(JSON.stringify(ex));
+                    fbLogin.reject();
                 }
-            });
+            }
+
+            function checkGrantedPermissions(autoDefer) {
+                var checkFunction = function (response) {
+                    if (angular.isDefined(response) && !angular.isDefined(response.error)) {
+                        var permissions = facebookPermissions.split(',');
+                        var allFound = true;
+                        angular.forEach(permissions, function (permission) {
+                            var found = false;
+                            angular.forEach(response.data, function (fbPermission) {
+                                if (permission === fbPermission.permission &&
+                                    (
+                                        fbPermission.status === 'granted' ||
+                                        fbPermission.status === 'declined'
+                                    )) {
+                                    found = true;
+                                }
+                            });
+                            if (!found) {
+                                allFound = false;
+                            }
+                        });
+                        if (allFound) {
+                            autoDefer.resolve(
+                                {
+                                    auto: true,
+                                    permissions: facebookPermissions
+                                }
+                            );
+                        } else {
+                            autoDefer.reject();
+                        }
+                    } else {
+                        autoDefer.reject();
+                    }
+                };
+                var graphPath = '/me/permissions';
+                if(angular.isDefined(cordovaFacebook)) {
+                    cordovaFacebook.api(graphPath, []).then(checkFunction, function(e) {
+                        console.log(JSON.stringify(e));
+                        autoDefer.reject();
+                    });
+                } else {
+                    FB.api(graphPath, checkFunction);
+                }
+            }
+
+            function canAutoLogin(autoDefer) {
+                try {
+                    var callback = function (response) {
+                        if (angular.isDefined(response) &&
+                            angular.isDefined(response.status) &&
+                            response.status === 'connected') {
+                            checkGrantedPermissions(autoDefer);
+                        } else {
+                            autoDefer.reject();
+                        }
+                    };
+                    if(angular.isDefined(cordovaFacebook)) {
+                        cordovaFacebook.getLoginStatus().then(callback, function(e) {
+                            console.log(JSON.stringify(e));
+                            autoDefer.reject();
+                        });
+                    } else {
+                        FB.getLoginStatus(callback);
+                    }
+                } catch (ex) {
+                    console.error(JSON.stringify(ex));
+                }
+            }
+
+            function inviteFriends(ids, message) {
+                var first = true;
+                var s = '';
+                angular.forEach(ids, function (id) {
+                    if (!first) {
+                        s = s + ', ';
+                    } else {
+                        first = false;
+                    }
+                    s = s + id;
+                });
+                var callback = function (response) {
+                    //  TODO - track?
+                    console.info(JSON.stringify(response));
+                };
+                var dialog = {
+                    method: 'apprequests',
+                    message: message,
+                    to: s
+                };
+                if(angular.isDefined(cordovaFacebook)) {
+                    cordovaFacebook.showDialog(dialog).then(callback, function() {
+                        //  TODO
+                    });
+                } else {
+                    FB.ui(dialog, callback);
+                }
+            }
+
+            function gameAndFacebookLoginMatch(player, matchDeferred) {
+                if (player.source === 'facebook') {
+                    try {
+                        var callback = function (response) {
+                            if (response.status === 'connected') {
+                                matchDeferred.resolve(response.authResponse.userID === player.sourceId);
+                            }
+                            else {
+                                matchDeferred.resolve(false);
+                            }
+                        };
+                        if(angular.isDefined(cordovaFacebook)) {
+                            cordovaFacebook.getLoginStatus().then(callback, function(e) {
+                                console.log(JSON.stringify(e));
+                                matchDeferred.resolve(false);
+                            });
+                        } else {
+                            FB.getLoginStatus(callback);
+                        }
+                    } catch (ex) {
+                        console.error(JSON.stringify);
+                        matchDeferred.resolve(false);
+                    }
+                } else {
+                    matchDeferred.resolve(false);
+                }
+            }
 
             return {
                 initiateFBLogin: function () {
                     var fbLogin = $q.defer();
                     loadFB().then(function () {
-                        try {
-                            FB.login(function (response) {
-                                if (angular.isDefined(response) &&
-                                    angular.isDefined(response.status) &&
-                                    response.status === 'connected') {
-                                    fbLogin.resolve({
-                                        auto: true,
-                                        permissions: facebookPermissions
-                                    });
-                                } else {
-                                    fbLogin.reject();
-                                }
-                            }, {scope: facebookPermissions});
-                        } catch(ex) {
-                            console.error(JSON.stringify(ex));
-                            fbLogin.reject();
-                        }
+                        login(fbLogin);
                     }, function () {
                         fbLogin.reject();
                     });
@@ -100,80 +233,17 @@ angular.module('coreGamesUi.services').factory('jtbFacebook',
                 },
                 canAutoSignIn: function () {
                     var autoDefer = $q.defer();
-                    loadFB().then(function (facebookPermissions) {
-                        try {
-                            FB.getLoginStatus(function (response) {
-                                if (angular.isDefined(response) &&
-                                    angular.isDefined(response.status) &&
-                                    response.status === 'connected') {
-                                    FB.api('/me/permissions', function (response) {
-                                            if (angular.isDefined(response) && !angular.isDefined(response.error)) {
-                                                var permissions = facebookPermissions.split(',');
-                                                var allFound = true;
-                                                angular.forEach(permissions, function (permission) {
-                                                    var found = false;
-                                                    angular.forEach(response.data, function (fbPermission) {
-                                                        if (permission === fbPermission.permission &&
-                                                            (
-                                                                fbPermission.status === 'granted' ||
-                                                                fbPermission.status === 'declined'
-                                                            )) {
-                                                            found = true;
-                                                        }
-                                                    });
-                                                    if (!found) {
-                                                        allFound = false;
-                                                    }
-                                                });
-                                                if (allFound) {
-                                                    autoDefer.resolve(
-                                                        {
-                                                            auto: true,
-                                                            permissions: facebookPermissions
-                                                        }
-                                                    );
-                                                } else {
-                                                    autoDefer.reject();
-                                                }
-                                            } else {
-                                                autoDefer.reject();
-                                            }
-                                        }
-                                    );
-                                } else {
-                                    autoDefer.reject();
-                                }
-                            });
-                        } catch(ex) {
-                            console.error(JSON.stringify(ex));
-                        }
+                    loadFB().then(function () {
+                        canAutoLogin(autoDefer);
                     }, function () {
                         autoDefer.reject();
                     });
                     return autoDefer.promise;
                 },
 
-                inviteFriends: function (ids) {
+                inviteFriends: function (ids, message) {
                     loadFB().then(function () {
-                        var first = true;
-                        var s = '';
-                        angular.forEach(ids, function (id) {
-                            if (!first) {
-                                s = s + ', ';
-                            } else {
-                                first = false;
-                            }
-                            s = s + id;
-                        });
-                        FB.ui({
-                                method: 'apprequests',
-                                message: 'Come play Twisted Hangman with me!',
-                                to: s
-                            },
-                            function (response) {
-                                //  TODO - track?
-                                console.info(JSON.stringify(response));
-                            });
+                        inviteFriends(ids, message);
                     }, function () {
                         //  TODO - alert error
                     });
@@ -181,23 +251,7 @@ angular.module('coreGamesUi.services').factory('jtbFacebook',
                 playerAndFBMatch: function (player) {
                     var matchDeferred = $q.defer();
                     loadFB().then(function () {
-                        if (player.source === 'facebook') {
-                            try {
-                                FB.getLoginStatus(function (response) {
-                                    if (response.status === 'connected') {
-                                        matchDeferred.resolve(response.authResponse.userID === player.sourceId);
-                                    }
-                                    else {
-                                        matchDeferred.resolve(false);
-                                    }
-                                });
-                            } catch(ex) {
-                                console.error(JSON.stringify);
-                                matchDeferred.resolve(false);
-                            }
-                        } else {
-                            matchDeferred.resolve(false);
-                        }
+                        gameAndFacebookLoginMatch(player, matchDeferred);
                     });
                     return matchDeferred.promise;
                 }
--- test/unit/core-games-ui/controllers/invite.js
+++ test/unit/core-games-ui/controllers/invite.js
@@ -2,44 +2,46 @@
 
 describe('Controller: CoreInviteCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('coreGamesUi.controllers'));
+    // load the controller's module
+    beforeEach(module('coreGamesUi.controllers'));
 
-  var InviteCtrl, scope;
+    var InviteCtrl, scope;
 
-  var invitableFriends = [{name: 'X', id: '1'}, {name: 'Y', id: '5'}, {name: 'Z', id: '10'}];
-  var facebookMock = {inviteFriends: jasmine.createSpy()};
-  var modalInstance = jasmine.createSpyObj('modalInstance', ['close', 'dismiss']);
+    var invitableFriends = [{name: 'X', id: '1'}, {name: 'Y', id: '5'}, {name: 'Z', id: '10'}];
+    var facebookMock = {inviteFriends: jasmine.createSpy()};
+    var modalInstance = jasmine.createSpyObj('modalInstance', ['close', 'dismiss']);
+    var aMessage = 'Come play frosty cones!';
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    invitableFriends = [];
-    scope = $rootScope.$new();
-    InviteCtrl = $controller('CoreInviteCtrl', {
-      $scope: scope,
-      jtbFacebook: facebookMock,
-      $modalInstance: modalInstance,
-      invitableFriends: function () {
-        return invitableFriends;
-      }
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope) {
+        invitableFriends = [];
+        scope = $rootScope.$new();
+        InviteCtrl = $controller('CoreInviteCtrl', {
+            $scope: scope,
+            jtbFacebook: facebookMock,
+            $modalInstance: modalInstance,
+            message: aMessage,
+            invitableFriends: function () {
+                return invitableFriends;
+            }
+        });
+    }));
+
+    it('initializes to friends and none chosen', function () {
+        expect(scope.invitableFriends()).toEqual(invitableFriends);
+        expect(scope.chosenFriends).toEqual([]);
     });
-  }));
 
-  it('initializes to friends and none chosen', function () {
-    expect(scope.invitableFriends()).toEqual(invitableFriends);
-    expect(scope.chosenFriends).toEqual([]);
-  });
+    it('cancel closes dialog', function () {
+        scope.cancel();
+        expect(modalInstance.dismiss).toHaveBeenCalled();
+        expect(facebookMock.inviteFriends).not.toHaveBeenCalled();
+    });
 
-  it('cancel closes dialog', function () {
-    scope.cancel();
-    expect(modalInstance.dismiss).toHaveBeenCalled();
-    expect(facebookMock.inviteFriends).not.toHaveBeenCalled();
-  });
-
-  it('invite invites chosen friend ids', function () {
-    scope.chosenFriends = [{name: 'X', id: '1'}, {name: 'A', id: '3'}];
-    scope.invite();
-    expect(modalInstance.close).toHaveBeenCalled();
-    expect(facebookMock.inviteFriends).toHaveBeenCalledWith(['1', '3']);
-  });
+    it('invite invites chosen friend ids', function () {
+        scope.chosenFriends = [{name: 'X', id: '1'}, {name: 'A', id: '3'}];
+        scope.invite();
+        expect(modalInstance.close).toHaveBeenCalled();
+        expect(facebookMock.inviteFriends).toHaveBeenCalledWith(['1', '3'], aMessage);
+    });
 });

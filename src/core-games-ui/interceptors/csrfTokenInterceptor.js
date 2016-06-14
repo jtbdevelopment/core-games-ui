b5c2644401ca5446fe8825da5aa55d22d5363562
JTB <jtbdevelopment@gmail.com>
2016-06-14 19:12:54 -0400

    added config entries to interceptors

--- src/core-games-ui/interceptors/csrfTokenInterceptor.js
+++ src/core-games-ui/interceptors/csrfTokenInterceptor.js
@@ -1,22 +1,26 @@
 'use strict';
 
-angular.module('coreGamesUi.interceptors').factory('jtbCSRFHttpInterceptor', function () {
-    var csrfToken;
-    return {
-        'request': function (config) {
-            if (angular.isDefined(csrfToken)) {
-                config.headers['X-XSRF-TOKEN'] = csrfToken;
+angular.module('coreGamesUi.interceptors')
+    .factory('jtbCSRFHttpInterceptor', function () {
+        var csrfToken;
+        return {
+            'request': function (config) {
+                if (angular.isDefined(csrfToken)) {
+                    config.headers['X-XSRF-TOKEN'] = csrfToken;
+                }
+                return config;
+            },
+            'response': function (response) {
+                if (angular.isDefined(response.headers) &&
+                    angular.isDefined(response.headers('XSRF-TOKEN')) &&
+                    response.headers('XSRF-TOKEN') !== null) {
+                    csrfToken = response.headers('XSRF-TOKEN');
+                }
+                return response;
             }
-            return config;
-        },
-        'response': function (response) {
-            if (angular.isDefined(response.headers) &&
-                angular.isDefined(response.headers('XSRF-TOKEN')) &&
-                response.headers('XSRF-TOKEN') !== null) {
-                csrfToken = response.headers('XSRF-TOKEN');
-            }
-            return response;
-        }
-    };
-});
+        };
+    })
+    .config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.interceptors.push('jtbCSRFHttpInterceptor');
+    }]);
 

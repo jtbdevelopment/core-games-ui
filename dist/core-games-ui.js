197e5d0c15ccc9ebf41b405260709a531a64dae2
JTB Development <jtbdevelopment@gmail.com>
2016-07-15 13:24:00 -0400

    removed invite control from core since boostrap dependent

--- dist/core-games-ui.js
+++ dist/core-games-ui.js
@@ -33,29 +33,6 @@
 
 'use strict';
 
-//  TODO - this is dependent on angular-bootstrap - this should get removed from this library
-angular.module('coreGamesUi.controllers').controller('CoreInviteCtrl',
-    ['$uibModalInstance', '$scope', 'invitableFriends', 'message', 'jtbFacebook',
-        function ($uibModalInstance, $scope, invitableFriends, message, jtbFacebook) {
-            $scope.invitableFriends = invitableFriends;
-            $scope.chosenFriends = [];
-            $scope.message = message;
-            $scope.invite = function () {
-                var ids = [];
-                angular.forEach($scope.chosenFriends, function (chosen) {
-                    ids.push(chosen.id);
-                });
-                jtbFacebook.inviteFriends(ids, message);
-                $uibModalInstance.close();
-            };
-            $scope.cancel = function () {
-                $uibModalInstance.dismiss();
-            };
-        }]);
-
-
-'use strict';
-
 angular.module('coreGamesUi.controllers')
     .controller('CoreSignInCtrl',
     ['$scope', '$window', '$cookies', 'jtbFacebook',

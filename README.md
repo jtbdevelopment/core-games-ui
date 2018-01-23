8ae33168d5eaa8b782777373a7c019db67ba3961
JTB <jtbdevelopment@gmail.com>
2018-01-22 20:33:43 -0500

    fix issue with Observerable/behavior

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 [![Build Status](https://travis-ci.org/jtbdevelopment/core-games-ui.svg?branch=master)](https://travis-ci.org/jtbdevelopment/core-games-ui)
 
 # core-games-ui
-Core javascript library for UI part to match core-games jvm
+Core TypeScript library for UI part to match core-games jvm
+
+Version < 3.0.0 are AngularJS
+Version >= 3.0.0 are Angular
--- src/core-games-ui/gamecache/abstract-turn-classifier.service.ts
+++ src/core-games-ui/gamecache/abstract-turn-classifier.service.ts
@@ -7,22 +7,24 @@ export abstract class AbstractTurnClassifier {
     protected static THEIR_TURN: string = 'Their Turn';
     protected static OLDER_GAMES: string = 'Older Games';
 
+    private static DEFAULT_CLASSIFICATIONS: BehaviorSubject<string[]> = new BehaviorSubject<string[]>(['Your Turn', 'Their Turn', 'Older Games']);
+
+    private static DEFAULT_ICONS: BehaviorSubject<Map<string, string>> = new BehaviorSubject<Map<string, string>>(new Map<string, string>(
+        [
+            [AbstractTurnClassifier.YOUR_TURN, 'play'],
+            [AbstractTurnClassifier.THEIR_TURN, 'pause'],
+            [AbstractTurnClassifier.OLDER_GAMES, 'stop']
+        ] as [string, string][]
+    ));
+
     //noinspection JSMethodCanBeStatic
     public getClassifications(): Observable<string[]> {
-        return new BehaviorSubject(['Your Turn', 'Their Turn', 'Older Games']);
+        return Observable.from(AbstractTurnClassifier.DEFAULT_CLASSIFICATIONS);
     }
 
     //  Game buckets to icons - can return empty map if not initialized
     //noinspection JSMethodCanBeStatic
     public getIcons(): Observable<Map<string, string>> {
-        return new BehaviorSubject(
-            new Map<string, string>(
-                [
-                    [AbstractTurnClassifier.YOUR_TURN, 'play'],
-                    [AbstractTurnClassifier.THEIR_TURN, 'pause'],
-                    [AbstractTurnClassifier.OLDER_GAMES, 'stop']
-                ] as [string, string][]
-            )
-        );
+        return Observable.from(AbstractTurnClassifier.DEFAULT_ICONS);
     }
 }

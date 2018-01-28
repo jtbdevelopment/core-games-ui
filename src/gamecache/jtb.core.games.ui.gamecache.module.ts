3de189169eee0715fbdcd8c69ef530a912031d71
JTB <jtbdevelopment@gmail.com>
2018-01-27 22:52:25 -0500

    fixed issue with missing model files

--- src/gamecache/jtb.core.games.ui.gamecache.module.ts
+++ src/gamecache/jtb.core.games.ui.gamecache.module.ts
@@ -4,6 +4,8 @@ import {JTBCoreGamesUIPhases} from '../phases/jtb.core.games.ui.phases.module';
 import {GameCacheService} from './game-cache.service';
 import {PhaseGameClassifier} from './phase-game-classifier.service';
 import {HttpClientModule} from '@angular/common/http';
+// noinspection ES6UnusedImports
+import {AbstractTurnClassifier} from './abstract-turn-classifier.service';
 
 @NgModule({
     imports: [JTBCoreGamesUIMessageBus, HttpClientModule, JTBCoreGamesUIPhases],
--- src/phases/jtb.core.games.ui.phases.module.ts
+++ src/phases/jtb.core.games.ui.phases.module.ts
@@ -2,6 +2,8 @@ import {NgModule} from '@angular/core';
 import {JTBCoreGamesUIMessageBus} from '../messagebus/jtb.core.games.ui.messagebus.module';
 import {PhaseCacheService} from './phase-cache.service';
 import {HttpClientModule} from '@angular/common/http';
+// noinspection ES6UnusedImports
+import {StandardPhases} from './standard-phases.model';
 
 @NgModule({
     imports: [HttpClientModule, JTBCoreGamesUIMessageBus],
